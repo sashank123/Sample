d3361f7d1bab8991dca7d6c929a37444c216cb9c
Shashank Vedicherla <vvsrshashank@gmail.com>
2016-10-27 14:06:51 +0530

    Adding two integers in c++

---
+++ AddingIntegers.cpp
@@ -0,0 +1,19 @@
+/*
+*	Author: Shashank 
+*	Date: October 27, 2016
+*	Introduction: Program about adding two integers and displaying the result on the console
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	/* Declaration of variables */
+  	int a,b;
+  	
+  	/* Accepting inputs from console */
+	cin>>a>>b;
+	
+	/* Writing output to the console */
+   	cout<<a+b;
+}
